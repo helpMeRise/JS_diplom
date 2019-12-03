7312594d37fe77a9447fba2eb16f2dc103498bb7
rises <rises128@gmail.com>
2019-12-03 15:47:53 +0500

    mask

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 'use strict';
 
-  import 'nodelist-foreach-polyfill';
-  import '@babel/polyfill';
-  import 'element-closest';
-  import elementClosest from 'element-closest';
-  elementClosest(window);
-  import 'es6-promise';
-  import 'fetch-polyfill';
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import 'element-closest';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'es6-promise';
+import 'fetch-polyfill';
 
 
-  import secondPhone from './modules/secondPhone';
-  import menu from './modules/menu';
-  import popups from './modules/popups';
+import secondPhone from './modules/secondPhone';
+import menu from './modules/menu';
+import popups from './modules/popups';
+import maskPhone from './modules/maskPhone';
 
 
 
 
-  //Показать/скрыть второй номер
-  secondPhone();
-  //Меню
-  menu();
-  //Вспливающие окна
-  popups();
+//Показать/скрыть второй номер
+secondPhone();
+//Меню
+menu();
+//Вспливающие окна
+popups();
+//Маска телефона
+maskPhone(`#feedback-input1`);
+maskPhone(`#feedback-input2`);
+maskPhone(`#feedback-input3`);
+maskPhone(`#feedback-input4`);
+maskPhone(`#feedback-input5`);
