ba61b5293fa16cbaf58a49cf78872dae114accb6
rises <rises128@gmail.com>
2019-12-08 16:26:14 +0500

    fuul list completed

--- src/modules/repairTypesPopup.js
+++ src/modules/repairTypesPopup.js
@@ -2,7 +2,7 @@ const repairTypesPopup = () => {
 
   const popup = document.querySelector(`.popup-repair-types`),
         navListPopupRepair = document.querySelector(`.nav-list-popup-repair`),
-        content = popup.querySelector(`.popup-repair-types-content`);
+        contentW = popup.querySelector(`.row`);
 
   document.body.addEventListener(`click`, (event) => {
     let target = event.target;
@@ -13,8 +13,17 @@ const repairTypesPopup = () => {
           if ( response.status !== 200 ) {
             throw new Error(`Ошибочка`);
           }
-          
+          let current;
           response.json().then(data => {
+            data.forEach( (item, i) => {
+              current = i;
+              let btn = document.createElement(`button`);
+              btn.classList.add(`button_o`, `popup-repair-types-nav__item`);
+              btn.textContent = item.title;
+              navListPopupRepair.appendChild(btn);
+
+              let content = document.createElement(`div`);
+              content.classList.add(`popup-repair-types-content`);
               let contentHead = document.createElement(`div`);
               contentHead.classList.add(`popup-repair-types-content__head`, `mobile-hide`);
               let title = document.createElement(`div`);
@@ -50,67 +59,69 @@ const repairTypesPopup = () => {
               let tdPp = document.createElement(`td`);
               tdPp.classList.add(`repair-types-value`);
 
-              
-            
-            
-            
-            popup.addEventListener(`click`, (event) => {
-              let target = event.target;
+              contentW.appendChild(content);
+              content.appendChild(contentHead);
+              title.textContent = item.title;
+              contentHead.appendChild(title);
+              date.textContent = new Date().toDateString();
+              contentHead.appendChild(date);
+              content.appendChild(tableWrap);
+              tableWrap.appendChild(tableHead);
+              tableHead.appendChild(thead);
+              thead.appendChild(trH);
+              thH.textContent = `Виды работ`;
+              trH.appendChild(thH);
+              thHE.textContent = `Ед. измерения`;
+              trH.appendChild(thHE);
+              thHC.textContent = `Цена за ед.`;
+              trH.appendChild(thHC);
+              tableWrap.appendChild(contentTable);
+              contentTable.appendChild(table);
+              table.appendChild(tbody);
 
-              data.forEach( (item) => {
-                
-                if ( tbody.children.length === item.priceList.length ) {
-                  return false;
-                }
-                if ( target.textContent === item.title) {
-                   
-                  content.appendChild(contentHead);
-                  title.textContent = item.title;
-                  contentHead.appendChild(title);
-                  date.textContent = new Date().toDateString();
-                  contentHead.appendChild(date);
-                  content.appendChild(tableWrap);
-                  tableWrap.appendChild(tableHead);
-                  tableHead.appendChild(thead);
-                  thead.appendChild(trH);
-                  thH.textContent = `Виды работ`;
-                  trH.appendChild(thH);
-                  thHE.textContent = `Ед. измерения`;
-                  trH.appendChild(thHE);
-                  thHC.textContent = `Цена за ед.`;
-                  trH.appendChild(thHC);
-                  tableWrap.appendChild(contentTable);
-                  contentTable.appendChild(table);
-                  table.appendChild(tbody);
-
-                  item.priceList.forEach( (elem) => {
-                    let tr = document.createElement(`tr`);
-                    tr.classList.add(`mobile-row`);
-                    let tdName = document.createElement(`td`);
-                    tdName.classList.add(`repair-types-name`);
-                    let tdTitle = document.createElement(`td`);
-                    tdTitle.classList.add(`mobile-col-title`, `tablet-hide`, `desktop-hide`);
-                    let tdPrice = document.createElement(`td`);
-                    tdPrice.classList.add(`mobile-col-title`, `tablet-hide`, `desktop-hide`);
-                    let tdTt = document.createElement(`td`);
-                    tdTt.classList.add(`repair-types-value`);
-                    let tdPp = document.createElement(`td`);
-                    tdPp.classList.add(`repair-types-value`);
-                    tbody.appendChild(tr);
-                    tdName.textContent = elem.typeService;
-                    tr.appendChild(tdName);
-                    tr.appendChild(tdTitle);
-                    tr.appendChild(tdPrice);
-                    tdTt.textContent = elem.units;
-                    tr.appendChild(tdTt);
-                    tdPp.textContent = elem.cost;
-                    tr.appendChild(tdPp);
-                  });
-                }
-                
+              item.priceList.forEach( (elem) => {
+                let tr = document.createElement(`tr`);
+                tr.classList.add(`mobile-row`);
+                let tdName = document.createElement(`td`);
+                tdName.classList.add(`repair-types-name`);
+                let tdTitle = document.createElement(`td`);
+                tdTitle.classList.add(`mobile-col-title`, `tablet-hide`, `desktop-hide`);
+                let tdPrice = document.createElement(`td`);
+                tdPrice.classList.add(`mobile-col-title`, `tablet-hide`, `desktop-hide`);
+                let tdTt = document.createElement(`td`);
+                tdTt.classList.add(`repair-types-value`);
+                let tdPp = document.createElement(`td`);
+                tdPp.classList.add(`repair-types-value`);
+                tbody.appendChild(tr);
+                tdName.textContent = elem.typeService;
+                tr.appendChild(tdName);
+                tr.appendChild(tdTitle);
+                tr.appendChild(tdPrice);
+                tdTt.textContent = elem.units;
+                tr.appendChild(tdTt);
+                tdPp.textContent = elem.cost;
+                tr.appendChild(tdPp);
               });
+
+              content.style.display = `none`;
             });
 
+            const contentBlocks = popup.querySelectorAll(`.popup-repair-types-content`),
+                  btns = popup.querySelectorAll(`.popup-repair-types-nav__item`);
+            contentBlocks[current].style.display = `block`;
+            popup.addEventListener(`click`, (event) => {
+              let target = event.target;
+              
+              btns.forEach( (item, i) => {
+                if (target === item) {
+                  contentBlocks[current].style.display = `none`;
+                  current = i;
+                  contentBlocks[current].style.display = `block`;
+                }
+              });
+            });
+            
+            
 
           });
         })
