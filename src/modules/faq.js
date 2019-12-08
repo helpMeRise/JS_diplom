336b602c711e02eaa24cc9fd11911908d0118545
rises <rises128@gmail.com>
2019-12-08 18:15:40 +0500

    faq

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -4,9 +4,11 @@ const faq = () => {
         titleBlocks = faq.querySelectorAll(`.title_block`),
         msg = faq.querySelectorAll(`.msg`);
 
+  msg.forEach( (item) => item.style.transition = `none` );
+  
   faq.addEventListener(`click`, (event) => {
     let target = event.target;
-    titleBlocks.forEach( (item) => {
+    titleBlocks.forEach( (item, i) => {
       if ( target === item ) {
         item.classList.toggle(`msg-active`);
       }
